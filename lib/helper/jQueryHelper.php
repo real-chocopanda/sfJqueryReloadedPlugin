b65c19e6fca80b7d41088c8a4a2de731b775bb12
John Benson <johnny@punkave.com>
2009-07-23 13:55:36 +0000

    updated the bundled UI js to the latest version
    git-svn-id: http://svn.symfony-project.com/plugins/sfJqueryReloadedPlugin/1.2/trunk@20443 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/helper/jQueryHelper.php
+++ lib/helper/jQueryHelper.php
@@ -44,9 +44,9 @@ function jq_add_plugins_by_name($args = array()) {
 
   $plugins = array(
     // Backwards compatibility
-    'sortable' => 'jquery-ui-1.7.1.custom.min.js',
+    'sortable' => 'jquery-ui-1.7.2.custom.min.js',
     
-    'ui' => 'jquery-ui-1.7.1.custom.min.js',
+    'ui' => 'jquery-ui-1.7.2.custom.min.js',
     'autocomplete' => 'jquery.autocomplete-1.0.2.min.js'
   );
 
